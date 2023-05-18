cd34ada4eb073baab60c757b3d6fcdff30882997
Popica <78389957+popica24@users.noreply.github.com>
2023-05-18 10:01:16 +0300

    Add project files.

---
+++ TaskManagementMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskManagementMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
